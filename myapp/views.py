81b832fdfcf34fdaff0134d133df1a9b7636bc2a
Ahmed Eid <ahmedeid2504@gmail.com>
2024-03-12 05:04:42 +0200

    moves form into counter page above the counter, adds start link in index page which redirects to counter page

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from datetime import datetime
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def counter(request):
-    text = request.POST['text']
-    amount_of_words = len(text.split())
-    return render(request, 'counter.html', {'amount': amount_of_words})
+    if request.method == "POST":
+        text = request.POST['text']
+        amount_of_words = len(text.split())
+        context={
+            'amount': amount_of_words,
+            }
+        return render(request, 'counter.html', context)
+    else:
+        return render(request, "counter.html")
