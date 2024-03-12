0abc96d03f704c58ae99c60bc18fb6adb7d4c747
Ahmed Eid <ahmedeid2504@gmail.com>
2024-03-12 03:54:45 +0200

    adds base html templates, base text counter page

---
+++ myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def counter(request):
+    text = request.GET['text']
+    return render(request, 'counter.html')
